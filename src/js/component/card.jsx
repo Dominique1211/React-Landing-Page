2640b5e8b45bb4da62d90409aca8713146e6f84d
Dominique1211 <sunshine.p2525@gmail.com>
2022-10-04 01:04:38 +0000

    correct className

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,72 +1,103 @@
 import React from "react";
 import "./cardstyles.css";
-import baby from "../../img/rigo-baby.jpg"
-const Card = ()=>{
-    return (
-        <div>
-            <div class="card">
-  {/* <div class="card-header">
+import baby from "../../img/rigo-baby.jpg";
+const Card = () => {
+  return (
+    <div classNameName="">
+      <div className="card">
+        {/* <div className="card-header">
     Featured
   </div> */}
-  <div class="card-body">
-    <h5 class="card-firsttitle">A Warm Welcome!</h5>
-    <p class="card-firsttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget turpis at ligula aliquet dapibus vitae vel leo. Nunc aliquam molestie justo, sed sollicitudin nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus suscipit lobortis leo vel tristique. Mauris eros leo, varius ac quam eu, porta pulvinar ligula. Vestibulum maximus ac ante vel maximus. Proin eleifend, mi non pharetra molestie, nunc ante scelerisque ante, tristique aliquet arcu lorem sit amet sem.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        <div className="card-body">
+          <h5 className="card-firsttitle">A Warm Welcome!</h5>
+          <p className="card-firsttext">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            eget turpis at ligula aliquet dapibus vitae vel leo. Nunc aliquam
+            molestie justo, sed sollicitudin nulla. Vestibulum ante ipsum primis
+            in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus
+            suscipit lobortis leo vel tristique. Mauris eros leo, varius ac quam
+            eu, porta pulvinar ligula. Vestibulum maximus ac ante vel maximus.
+            Proin eleifend, mi non pharetra molestie, nunc ante scelerisque
+            ante, tristique aliquet arcu lorem sit amet sem.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
 
-<div class="">
-<div class="container text-center">
-  <div class="row row-cols-4">
-    <div class="col">
-    <div className="all-cards">
-  <img src={baby} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
+      <div className="">
+        <div className="container text-center">
+          <div className="row row-cols-4">
+            <div className="col">
+              <div classNameName="all-cards">
+                <img src={baby} classNameName="card-img-top" alt="..." />
+                <div classNameName="card-body">
+                  <h5 classNameName="card-title">Card title</h5>
+                  <p classNameName="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" classNameName="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
 
-<div class="col">
-    <div className="all-cards">
-  <img src={baby} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
+            <div className="col">
+              <div classNameName="all-cards">
+                <img src={baby} classNameName="card-img-top" alt="..." />
+                <div classNameName="card-body">
+                  <h5 classNameName="card-title">Card title</h5>
+                  <p classNameName="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" classNameName="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
 
-<div class="col">
-    <div className="all-cards">
-  <img src={baby} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
+            <div className="col">
+              <div classNameName="all-cards">
+                <img src={baby} classNameName="card-img-top" alt="..." />
+                <div classNameName="card-body">
+                  <h5 classNameName="card-title">Card title</h5>
+                  <p classNameName="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" classNameName="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
 
-<div class="col">
-    <div className="all-cards">
-  <img src={baby} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-</div>
+            <div className="col">
+              <div classNameName="all-cards">
+                <img src={baby} classNameName="card-img-top" alt="..." />
+                <div classNameName="card-body">
+                  <h5 classNameName="card-title">Card title</h5>
+                  <p classNameName="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" classNameName="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
 
-</div>
-</div>
-</div>
-    )
-}
-
-export {Card}
+export { Card };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,6 +12,14 @@ const Home = () => {
 			<Navbar/>
 			<Card/>
 			{/* <Cardtwo/> */}
+			<footer className="bg-light text-center text-lg-start">
+				<div className="text-center p-3">
+					© 2020 Copyright:
+					<a className="text-dark" href="https://mdbootstrap.com/">
+						MDBootstrap.com
+					</a>
+				</div>
+			</footer>
 		</div>
 	);
 };
